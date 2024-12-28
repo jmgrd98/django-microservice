e4d8cc366d665700da91c80c8196571cb5da0353
jmgrd98 <jmgrd98@gmail.com>
2024-12-28 19:09:49 -0300

    commit

---
+++ admin/products/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductsSerializer
+from rest_framework import status
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request): #/api/products
+        products = Product.objects.all()
+        serializer = ProductsSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def create(self, request): #/api/products
+        serializer = ProductsSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def retrieve(self, request, pk=None): # /api/products/:id
+        product = Product.objects.get(id=pk)
+        serializer = ProductsSerializer(product)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def update(self, request, pk=None): # /api/products/:id
+        product = Product.objects.get(id=pk)
+        serializer = ProductsSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
+    def delete(self, request, pk=None): # /api/products/:id
+        product = Product.objects.get(pk=pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
